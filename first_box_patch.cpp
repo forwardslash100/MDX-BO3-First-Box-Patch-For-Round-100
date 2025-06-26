bf7bd48d92df4614b80d590c9ec1cabb37ba4ce6
forwardslash100 <forbackslash@gmail.com>
2025-06-26 23:48:05 +0100

    Add files via upload

---
+++ first_box_patch.cpp
@@ -0,0 +1,201 @@
+else if(level.script == "zm_zod") // Shadows Of Evil
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "octobomb" ) ) ) // Lil' Arnies
+				return GetWeapon( "octobomb" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_standard" ) ) ) // Kuda
+				return GetWeapon( "smg_standard" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_cqb" ) ) ) // HVK-30
+				return GetWeapon( "ar_cqb" );
+		}
+
+
+
+
+
+				else if(level.script == "zm_factory") // The Giant
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "tesla_gun" ) ) ) // Wunderwaffe DG-2
+				return GetWeapon( "tesla_gun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+				return GetWeapon( "cymbal_monkey" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_cqb" ) ) ) // HVK-30
+				return GetWeapon( "ar_cqb" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_castle") // Der Eisendrache
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "shotgun_semiauto" ) ) ) // 205 Brecci
+				return GetWeapon( "shotgun_semiauto" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+				return GetWeapon( "cymbal_monkey" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_cqb" ) ) ) // HVK-30
+				return GetWeapon( "ar_cqb" );
+		}
+
+
+
+
+		
+						else if(level.script == "zm_island") // Zetsubou No Shima
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_fastfire" ) ) ) // Vesper
+				return GetWeapon( "smg_fastfire" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_accurate" ) ) ) // ICR-1
+				return GetWeapon( "ar_accurate" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_capacity" ) ) ) // Weevil
+				return GetWeapon( "smg_capacity" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+		        return GetWeapon( "cymbal_monkey" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_stalingrad") // Gorod Krovi
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "raygun_mark3" ) ) ) // GKZ-45 Mk3
+				return GetWeapon( "raygun_mark3" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_fastfire" ) ) ) // Vesper
+				return GetWeapon( "smg_fastfire" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_cqb" ) ) ) // HVK-30
+				return GetWeapon( "ar_cqb" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_genesis") // Revelations
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "idgun_genesis_0" ) ) ) // Apothicon Servant
+				return GetWeapon( "idgun_genesis_0" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "thundergun" ) ) ) // Thundergun
+				return GetWeapon( "thundergun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "octobomb" ) ) ) // Lil' Arnies
+				return GetWeapon( "octobomb" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "hero_gravityspikes_melee" ) ) ) // Ragnarok DG-4
+			    return GetWeapon( "hero_gravityspikes_melee" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_prototype") // Nacht Der Untoten
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "thundergun" ) ) ) // Thundergun
+				return GetWeapon( "thundergun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+				return GetWeapon( "cymbal_monkey" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_standard" ) ) ) // Kuda
+				return GetWeapon( "smg_standard" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_asylum") // Verruckt
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "tesla_gun" ) ) ) // Wunderwaffe DG-2
+				return GetWeapon( "tesla_gun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+				return GetWeapon( "cymbal_monkey" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "raygun_mark2" ) ) ) // Ray Gun Mark II
+				return GetWeapon( "raygun_mark2" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_sumpf") // Shi No Numa
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "tesla_gun" ) ) ) // Wunderwaffe DG-2
+				return GetWeapon( "tesla_gun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+				return GetWeapon( "cymbal_monkey" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "raygun_mark2" ) ) ) // Ray Gun Mark II
+				return GetWeapon( "raygun_mark2" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_theater") // Kino Der Toten
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "thundergun" ) ) ) // Thundergun
+				return GetWeapon( "thundergun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+				return GetWeapon( "cymbal_monkey" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_versatile" ) ) ) // VMP
+				return GetWeapon( "smg_versatile" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "pistol_fullauto" ) ) ) // L-CAR 9
+			    return GetWeapon( "pistol_fullauto" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_cosmodrone") // Ascension
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "thundergun" ) ) ) // Thundergun
+				return GetWeapon( "thundergun" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "black_hole_bomb" ) ) ) // Gersh Device
+				return GetWeapon( "black_hole_bomb" );
+		}
+
+
+
+
+
+						else if(level.script == "zm_temple") // Shangri-La
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "shrink_ray" ) ) ) // 31-79 JGb215
+				return GetWeapon( "shrink_ray" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_fastfire" ) ) ) // Vesper
+				return GetWeapon( "smg_fastfire" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_cqb" ) ) ) // HVK-30
+				return GetWeapon( "ar_cqb" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "cymbal_monkey" ) ) ) // Cymbal Monkeys
+			    return GetWeapon( "cymbal_monkey" );
+		}
+
+
+
+
+
+				else if(level.script == "zm_moon") // Moon
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "microwavegundw" ) ) ) // Wave Gun
+				return GetWeapon( "microwavegundw" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "quantum_bomb" ) ) ) // Quantum Entanglement Device
+				return GetWeapon( "quantum_bomb" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_standard" ) ) ) // Kuda
+				return GetWeapon( "smg_standard" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_capacity" ) ) ) // Weevil
+				return GetWeapon( "smg_capacity" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "black_hole_bomb" ) ) ) // Gersh Device
+				return GetWeapon( "black_hole_bomb" );	
+		}
+
+
+
+
+
+						else if(level.script == "zm_tomb") // Origins
+		{
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "raygun_mark2" ) ) ) // Ray Gun Mark II
+				return GetWeapon( "raygun_mark2" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "smg_standard" ) ) ) // Kuda
+				return GetWeapon( "smg_standard" );
+			if(!player zm_weapons::has_weapon_or_upgrade( GetWeapon( "ar_cqb" ) ) ) // HVK-30
+				return GetWeapon( "ar_cqb" );
+		}
